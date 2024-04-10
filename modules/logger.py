d3e8aa367a8e37ea7849b01e86abbe6f32a8e382
Alex Socarras <alexsocarras@Alexs-MacBook-Pro.local>
2024-04-10 19:08:49 -0400

    remove file_handler from logger

--- modules/logger.py
+++ modules/logger.py
@@ -13,13 +13,13 @@ stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
-file_handler = logging.FileHandler(LOG_FILE)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+# file_handler = logging.FileHandler(LOG_FILE)
+# file_handler.setFormatter(formatter)
+# logger.addHandler(file_handler)
 
 ## For writing formatting text to the logfile for enhanced human readability
-def log_format(text:str):
-    """Writes any string to the log file and console without additional logger formatting."""
-    with open(LOG_FILE, "a") as file:
-        file.write(str(text) + "\n")
-    print(str(text) + "\n")
+# def log_format(text:str):
+#     """Writes any string to the log file and console without additional logger formatting."""
+#     with open(LOG_FILE, "a") as file:
+#         file.write(str(text) + "\n")
+#     print(str(text) + "\n")
